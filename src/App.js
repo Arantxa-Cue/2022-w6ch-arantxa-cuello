3cc7e3da244e1558c5ac29af246ee779ddbeec5a
Arantxa Cuello <arantxa_312@hotmail.com>
2022-02-20 14:59:29 +0100

    route added and test ammended

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import RobotsPage from "./pages/RobotsPage/RobotsPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
       <h1>Robots</h1>
       <Routes>
         <Route path="/robots" element={<RobotsPage />} />
+        <Route path="/" element={<Navigate to="/robots" />} />
       </Routes>
     </>
   );
